fcdb9c7cc6a49f88397d7e42e55b326bf2e1d30e
KASSANDRA SMITH <kassandrasmith@utexas.edu>
2015-06-07 15:37:49 -0500

    Use abstraction layer
    Add conveniently named color system

--- src/main.c
+++ src/main.c
@@ -20,6 +20,9 @@
 #define rightbutton (GPIO_PORTE_DATA_R & 0x04)
 #define rightpushed 0x04
 
+#define BLACK 0x0000
+#define WHITE 0xFFFF
+
 //declare global variables
 uint16_t score;
 uint16_t time;
@@ -54,7 +57,7 @@ int main(void) {
 
     IntMasterEnable();                                         //end of initializations, enable interrupts
     //initial state for screen
-    fill_background(0x0000);
+    fill_background(BLACK);
     // We need to stall here and wait for systick to trigger the game
     // loop at regular intervals.
     while(true);
@@ -130,11 +133,8 @@ void check (void){
 }
 
 void render (void) {
-    ST7735_SetCursor(1, 1); //Set cursor to top left
     write_score(score);
-    ST7735_SetCursor(1, 2); //FIXME set cursor to write below score
     write_time(time);
-    ST7735_SetCursor(1, 3); ///FIXME set cursor to write below time
     write_fuel(fuel);
 
     draw_bitmap(xposit, altitude, lander, 7, 9);
@@ -143,25 +143,20 @@ void render (void) {
 
 //Output some sort of death message
 void die(DeathType_t deathtype) {
-    ST7735_SetCursor(40, 1); ///FIXME set cursors
-    ST7735_OutString("You died!");
+
+    draw_string(40, 0, "You died!", WHITE);
     switch (deathtype) {
         case CRASHED:
-            ST7735_SetCursor(40, 40);
-            ST7735_OutString("Lost 20 fuel units.");
+            draw_string(40, 40, "Lost 20 fuel units.", WHITE);
             fuel =- 20;
-            ST7735_SetCursor(40, 60);
-            ST7735_OutString("Fuel left:");
-            ST7735_SetCursor(40, 80);
+            draw_string(40, 60, "Fuel left:", WHITE);
             write_fuel(fuel);
-            ST7735_SetCursor(40, 100);
-            ST7735_OutString("Current score:");
+            draw_string(40, 80, "Current score:", WHITE);
             write_score(score);
             break;
         case OUTOFTIME:
-            ST7735_SetCursor(40, 40);
-            ST7735_OutString("Out of time!");
-            ST7735_OutString("Final score:");
+            draw_string(40, 40, "Out of time!", WHITE);
+            draw_string(40, 60, "Final score:", WHITE);
             write_score(score);
             //TODO write a slight delay
             break;
@@ -169,9 +164,9 @@ void die(DeathType_t deathtype) {
 }
 
     void land() {
-        draw_string(40, 20, "You landed!", 0xFFFF);
+        draw_string(40, 20, "You landed!", WHITE);
     if(fuel>0){
-        draw_string(40, 40, "Fuel remaining:", 0xFFFF);
+        draw_string(40, 40, "Fuel remaining:", WHITE);
         write_fuel(fuel);
         //TODO write a slight delay
     }
